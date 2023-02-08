43e572be5b1f19d99abc3acf74624cbab978f72d
JNK <deal6789@naver.com>
2023-02-08 23:44:22 +0900

    mission 02

--- main.py
+++ main.py
@@ -8,61 +8,96 @@ class Graph():
 def print_graph(g):
 	print('	', end='')
 	for v in range(g.SIZE):
-		print("%9s" % store_array[v][0], end =' ')
+		print(city_array[v], end =' ')
 	print()
 	for row in range(g.SIZE):
-		print("%9s" % store_array[row][0], end =' ')
+		print(city_array[row], end =' ')
 		for col in range(g.SIZE):
-			print("%8d" % g.graph[row][col], end = ' ')
+			print("%2d" % g.graph[row][col], end = ' ')
 		print()
 	print()
 
 
-G1 = None
-store_array = [['GS25', 30], ['CU', 60], ['Seven11', 10], ['MiniStop', 90], ['Emart24', 40]]
-GS25, CU, Seven11, MiniStop, Emart24 = 0, 1, 2, 3, 4
+def find_vertex(g, find_vtx):
+	stack = []
+	visited_array = []
 
-g_size = 5
+	current = 0
+	stack.append(current)
+	visited_array.append(current)
+
+	while len(stack) != 0:
+		next = None
+		for vertex in range(g_size):
+			if g.graph[current][vertex] != 0:
+				if vertex in visited_array:
+					pass
+				else:
+					next = vertex
+					break
+
+		if next != None:
+			current = next
+			stack.append(current)
+			visited_array.append(current)
+		else :
+			current = stack.pop()
+
+	if find_vtx in visited_array:
+		return True
+	else:
+		return False
+
+G1 = None
+city_array = ['서울', '뉴욕', '런던', '북경', '방콕', '파리']
+서울, 뉴욕, 런던, 북경, 방콕, 파리 = 0, 1, 2, 3, 4, 5
+
+
+g_size = 6
 G1 = Graph(g_size)
 
-# 편의점 인접행렬
-G1.graph[GS25][CU] = 1; G1.graph[GS25][Seven11] = 1
-G1.graph[CU][GS25] = 1; G1.graph[CU][Seven11] = 1; G1.graph[CU][MiniStop] = 1
-G1.graph[Seven11][GS25] = 1; G1.graph[Seven11][CU] = 1; G1.graph[Seven11][MiniStop] = 1
-G1.graph[MiniStop][Seven11] = 1; G1.graph[MiniStop][CU] = 1; G1.graph[MiniStop][Emart24] = 1
-G1.graph[Emart24][MiniStop] = 1
+# 지도를 그래프로 구현
+G1.graph[서울][뉴욕] = 80; G1.graph[서울][북경] = 10
+G1.graph[뉴욕][서울] = 80; G1.graph[뉴욕][북경] = 40; G1.graph[뉴욕][방콕] = 70
+G1.graph[런던][방콕] = 30; G1.graph[런던][파리] = 60
+G1.graph[북경][서울] = 10; G1.graph[북경][뉴욕] = 40; G1.graph[북경][방콕] = 50
+G1.graph[방콕][뉴욕] = 70; G1.graph[방콕][북경] = 50; G1.graph[방콕][런던] = 30; G1.graph[방콕][파리] = 20
+G1.graph[파리][방콕] = 20; G1.graph[파리][런던] = 60;
 
-print('## 편의점 그래프 ##')
+print('## 해저 케이블 연결을 위한 전체 연결도 ##')
 print_graph(G1)
 
-stack = []
-visited_array = []			# 방문한 편의점
+edge_array = []
+for i in range(g_size):
+	for k in range(g_size):
+		if G1.graph[i][k] != 0:
+			edge_array.append([G1.graph[i][k], i, k])
 
-current = 0			# 시작 편의점
-max_store = current		# 최대 개수 편의점 번호(GS25)
-max_count = store_array[current][1]	# 편의점의 허니버터 숫자
-stack.append(current)
-visited_array.append(current)
+from operator import  itemgetter
+edge_array = sorted(edge_array, key = itemgetter(0), reverse=False)
 
-# 깊이 우선 탐색 방식
-while (len(stack) != 0):
-	next = None
-	for vertex in range(g_size):
-		if G1.graph[current][vertex] == 1:
-			if vertex in visited_array:	# 방문한 적이 있는 편의점이면 탈락
-				pass
-			else:			# 방문한 적이 없는 편의점이면 다음 편의점으로 지정
-				next = vertex
-				break
+new_array = []
+for i in range(0, len(edge_array), 2):
+	new_array.append(edge_array[i])
 
-	if next != None:				# 방문할 다음 편의점이 있는 경우
-		current = next
-		stack.append(current)
-		visited_array.append(current)
-		if store_array[current][1] > max_count:
-			max_count = store_array[current][1]
-			max_store = current
-	else:					# 방문할 다음 편의점이 없는 경우
-		current = stack.pop()
+index = 0
+while len(new_array) > g_size - 1:	# 간선의 개수가 '정점 개수-1'일 때까지 반복
+	start = new_array[index][1]
+	end = new_array[index][2]
+	save_cost = new_array[index][0]
 
-print('허니버터칩 최대 보유 편의점(개수) -->', store_array[max_store][0], '(', store_array[max_store][1], ')')
+	G1.graph[start][end] = 0
+	G1.graph[end][start] = 0
+
+	start_YN = find_vertex(G1, start)
+	end_YN = find_vertex(G1, end)
+
+	if start_YN and end_YN:
+		del (new_array[index])
+	else:
+		G1.graph[start][end] = save_cost
+		G1.graph[end][start] = save_cost
+		index += 1
+
+print('## 가장 효율적인 해저 케이블 연결도 ##')
+print_graph(G1)
